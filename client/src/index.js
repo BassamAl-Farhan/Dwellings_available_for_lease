d6e24224c9ce46524bf693ddc83f884d4724201c
Davood Keshmiri <davoodkeshmiri@gmail.com>
2023-05-18 15:58:24 -0500

    new changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import reportWebVitals from "./reportWebVitals";
+const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL,
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router history={history}>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/pages/popular.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Popular = () => {
+  return (
+    <>
+      <div class="header">
+        <h2>Popular places to Buy</h2>
+        <button id="home-btn">Home</button>
+      </div>
+      <div class="main-div">
+        <div class="prop-div">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="Home pic"
+            width="100"
+            height="100"
+          />
+          <h4>Home Info</h4>
+          <button id="add-button">Add to Library</button>
+        </div>
+        <div class="prop-div">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="Home pic"
+            width="100"
+            height="100"
+          />
+          <h4>Home Info</h4>
+          <button id="add-button">Add to Library</button>
+        </div>
+        <div class="prop-div">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="Home pic"
+            width="100"
+            height="100"
+          />
+          <h4>Home Info</h4>
+          <button id="add-button">Add to Library</button>
+        </div>
+        <div class="prop-div">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="Home pic"
+            width="100"
+            height="100"
+          />
+          <h4>Home Info</h4>
+          <button id="add-button">Add to Library</button>
+        </div>
+        <div class="prop-div">
+          <img
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="Home pic"
+            width="100"
+            height="100"
+          />
+          <h4>Home Info</h4>
+          <button id="add-button">Add to Library</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Popular;
